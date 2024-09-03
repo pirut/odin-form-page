36e45ce1f5d693719c7a1ea1b94ef128dccaf32e
JR Bussard <scottbussardjr@gmail.com>
2024-09-02 20:25:44 -0400

    Added a bunch of css classes and a title for the page
    I dont even remember all the things that I did during this commit because I did this over multiple days. I added a bunch of styling and added a title for the main page. I also adjsuted the JS for the paralax effect.

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ let y = 0;
 
 document.addEventListener("mousemove", parallax);
 function parallax(event) {
-    x = (window.innerWidth - event.pageX) / 90;
-    y = (window.innerHeight - event.pageY) / 90;
+    x = (window.innerWidth - event.pageX) / 70;
+    y = (window.innerHeight - event.pageY) / 70;
 
     bgImg.style.transform = `translateX(${x}px) translateY(${y}px) scale(150%)`;
 }
